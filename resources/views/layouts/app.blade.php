66aa392782ff5445bda3dee224e49c6cbdf68620
guapi <franky.xu@qibaozz.com>
2016-12-08 11:54:40 +0800

    add the umeng

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
     <script src="{{ elixir('js/app.js') }}"></script>
 @endif
 @yield('script')
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260952791'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1260952791' type='text/javascript'%3E%3C/script%3E"));</script>
 </body>
 
 <script>
