4660c9ce46af0c85ae6bcfc58153985d0144888c
guapi <franky.xu@qibaozz.com>
2016-12-20 21:11:45 +0800

    fix

--- resources/views/azhi/azhi.blade.php
+++ resources/views/azhi/azhi.blade.php
@@ -15,7 +15,11 @@
 </head>
 
 <body>
+<div style="text-align: center">
+    <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=28285910&auto=0&height=32"></iframe>
+</div>
 <div id="mainDiv">
+    <div style="te"></div>
     <div id="content" style="text-align: center;width: 100%" >
         {{--
 
