aac2f44359b6d3cb7172409a9d1b1809b8c219df
guapi <franky.xu@qibaozz.com>
2017-03-16 20:19:17 +0800

    fix the footer about the beian

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
-                    <span><a href="www.miitbeian.gov.cn" target="_blank">粤ICP备16053280号-1</a></span> |
+                    <span><a href="http://www.miitbeian.gov.cn" target="_blank">粤ICP备16053280号-1</a></span> |
                     <span><a href="https://github.com/guapier"><i class="fa fa-github fa-fw"></i></a> </span>
                 </div>
             </div>
